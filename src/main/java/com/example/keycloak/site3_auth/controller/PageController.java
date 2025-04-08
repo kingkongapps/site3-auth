65fbf01d12647cb8773084a9d6649370d60cfce7
inmokang <inmo.kang@gmail.com>
2025-04-08 22:02:53 +0900

    site url 변수처리...

--- src/main/java/com/example/keycloak/site3_auth/controller/PageController.java
+++ src/main/java/com/example/keycloak/site3_auth/controller/PageController.java
@@ -1,61 +1,116 @@
 package com.example.keycloak.site3_auth.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+
+    @Value("${site.url1}")
+    private String siteUrl1;
+    @Value("${site.url2}")
+    private String siteUrl2;
+    @Value("${site.url3}")
+    private String siteUrl3;
+
     @GetMapping(value = "/")
-    public String goHome() {
+    public String goHome(Model model) {
         System.out.println("goHome()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/home";
     }
 
     @GetMapping(value = "/admin/admin-test1")
-    public String goAdminTest1() {
+    public String goAdminTest1(Model model) {
         System.out.println("goAdminTest1()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/admin/admin-test1";
     }
 
     @GetMapping(value = "/admin/admin-test2")
-    public String goAdminTest2() {
+    public String goAdminTest2(Model model) {
         System.out.println("goAdminTest2()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/admin/admin-test2";
     }
 
     @GetMapping(value = "/user/user-test1")
-    public String goUserTest1() {
+    public String goUserTest1(Model model) {
         System.out.println("goUserTest1()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/user/user-test1";
     }
 
     @GetMapping(value = "/user/user-test2")
-    public String goUserTest2() {
+    public String goUserTest2(Model model) {
         System.out.println("goUserTest2()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/user/user-test2";
     }
 
     @GetMapping(value = "/login")
-    public String goLogin() {
+    public String goLogin(Model model) {
         System.out.println("goLogin()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/login/login";
     }
 
     @GetMapping(value = "/token")
-    public String goToken() {
+    public String goToken(Model model) {
         System.out.println("goToken()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/login/token";
     }
 
     @GetMapping(value = "/login/profile")
-    public String goProfile() {
+    public String goProfile(Model model) {
         System.out.println("goProfile()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/login/token";
     }
 
     @GetMapping(value = "/jwt-decode")
-    public String goJWTDecode() {
+    public String goJWTDecode(Model model) {
         System.out.println("goJWTDecode()...");
+
+        model.addAttribute("SITE_URL1", siteUrl1);
+        model.addAttribute("SITE_URL2", siteUrl2);
+        model.addAttribute("SITE_URL3", siteUrl3);
+
         return "/login/jwt-decode";
     }
 }
