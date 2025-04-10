ff1a95a2ffd7b9e848e85e77886c00be7cf34ec9
inmokang <inmo.kang@gmail.com>
2025-04-10 12:16:31 +0900

    SecurityContextHolder에서 accessToken꺼내는 부분 수정...

--- src/main/java/com/example/keycloak/site3_auth/controller/TokenRestController.java
+++ src/main/java/com/example/keycloak/site3_auth/controller/TokenRestController.java
@@ -7,6 +7,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,14 +30,22 @@ public class TokenRestController {
         try {
             String userId = (String) oidcUser.getUserInfo().getClaims().get("preferred_username");
             String userName = (String) oidcUser.getUserInfo().getClaims().get("name");
-            String accessToken = oidcUser.getIdToken().getTokenValue();
-            String refreshToken = getRefreshToken(authentication);
+
+            //
+            OAuth2AuthorizedClient auth2AuthorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient(((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId(), authentication.getName());
+            OAuth2AccessToken oAuth2AccessToken = auth2AuthorizedClient.getAccessToken();
+            OAuth2RefreshToken oAuth2RefreshToken = auth2AuthorizedClient.getRefreshToken();
+
+            String idToken = oidcUser.getIdToken().getTokenValue();
+            String accessToken = oAuth2AccessToken.getTokenValue();
+            String refreshToken = oAuth2RefreshToken.getTokenValue();
             //
             result.put("result", "OK");
             result.put("msg", "SUCCESS");
             result.put("userId", userId);
             result.put("userName", userName);
-            result.put("token", accessToken);
+            result.put("id_token", idToken);
+            result.put("access_token", accessToken);
             result.put("refresh_token", refreshToken);
         } catch (Exception e) {
             result.put("result", "NOT_OK");
@@ -44,15 +55,15 @@ public class TokenRestController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    private String getRefreshToken(Authentication authentication) {
-        OidcUser oidcUser = (OidcUser)  authentication.getPrincipal();
-        String clientRegistrationId = "keycloak";
-        OAuth2AuthorizedClient client = oAuth2AuthorizedClientService.loadAuthorizedClient(clientRegistrationId, oidcUser.getName());
-
-        if( client != null) {
-            return client.getRefreshToken().getTokenValue();
-        }
-
-        return null;
-    }
+//    private String getRefreshToken(Authentication authentication) {
+//        OidcUser oidcUser = (OidcUser)  authentication.getPrincipal();
+//        String clientRegistrationId = "keycloak";
+//        OAuth2AuthorizedClient client = oAuth2AuthorizedClientService.loadAuthorizedClient(clientRegistrationId, oidcUser.getName());
+//
+//        if( client != null) {
+//            return client.getRefreshToken().getTokenValue();
+//        }
+//
+//        return null;
+//    }
 }
